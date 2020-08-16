a9f8344343b45553fb8bbc24bd377086d448dfa3
Ross Hoyt <rosshoytmusic@gmail.com>
2020-08-16 16:41:22 -0700

    implemented delete list item(s)

--- main.js
+++ main.js
@@ -85,7 +85,10 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'Clear Items'
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {
                 label: 'Quit',
