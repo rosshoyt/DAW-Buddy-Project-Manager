5306536f238b6c8b04e6944f2fa9214043076c1a
Ross Hoyt <rosshoytmusic@gmail.com>
2020-09-15 23:55:20 -0700

    moved dynamic html table methods to table-utils.js

--- src/common/string-utils.js
+++ src/common/string-utils.js
@@ -4,4 +4,14 @@ function removeAllPrecedingDirectories(fullPath) {
     // TODO platform-dependant parsing of /, \ etc
     return fullPath.substring(fullPath.lastIndexOf("/") + 1, fullPath.length);
 }
-module.exports = { removeAllPrecedingDirectories }
+
+function getReadableFileSizeString(fileSizeInBytes) {
+    var i = -1;
+    var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+    do {
+      fileSizeInBytes = fileSizeInBytes / 1024;
+      i++;
+    } while (fileSizeInBytes > 1024);
+    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+  };
+module.exports = { getReadableFileSizeString, removeAllPrecedingDirectories }
