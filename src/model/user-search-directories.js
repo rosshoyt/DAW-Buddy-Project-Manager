c9519790341332c19d31162fc20056c445a09fb6
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-03-28 20:42:01 -0700

    Implemented domain model class UserSearchDirectories

---
+++ src/model/user-search-directories.js
@@ -0,0 +1,6 @@
+// domain class which holds the user's search directories for music projects
+module.exports = class UserSearchDirectories{
+    constructor(){
+        this.userSearchDirectories = [];
+    }
+}
